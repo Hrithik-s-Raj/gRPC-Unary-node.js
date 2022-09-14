5a30e9f59b599f62a3f1957ebbfc05b1485a17cd
hrithik2000 <rhrithik1@gmail.com>
2022-09-14 10:44:13 +0530

    gRPC Unary

---
+++ greet/client/index.js
@@ -0,0 +1,28 @@
+const grpc = require("@grpc/grpc-js");
+const { GreetServiceClient } = require("../proto/greet_grpc_pb");
+const { GreetRequest } = require("../proto/greet_pb");
+
+function doGreet(client) {
+  console.log("unary grpc API invoked Succesfully");
+  const req = new GreetRequest().setFirstName("Hrithik.s.raj");
+
+  client.greet(req, (err, res) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log(`Greet:${res.getResult()}`);
+  });
+}
+
+function main() {
+  const creds = grpc.ChannelCredentials.createInsecure();
+
+  const client = new GreetServiceClient("localhost:50051", creds);
+
+  doGreet(client);
+
+  client.close();
+}
+
+main();
